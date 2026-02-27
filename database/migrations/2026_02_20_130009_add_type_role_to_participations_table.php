0fc00f88c6b36be6161887127bf21e7edc6925e4
chahdR <chahddev@gmail.com>
2026-02-27 11:27:50 +0000

    Add files via upload

---
+++ database/migrations/2026_02_20_130009_add_type_role_to_participations_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::table('participations', function (Blueprint $table) {
+            $table->enum('typeRole', ['principal', 'secondaire'])
+                  ->after('role');
+        });
+    }
+
+
+  public function down(): void
+    {
+        Schema::table('participations', function (Blueprint $table) {
+            $table->dropColumn('typeRole');
+        });
+    }
+};
